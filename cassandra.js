2e0b77f60fcc4d83ef5b4bd51222f8980785459f
GunasekharY <34928352+GunasekharY@users.noreply.github.com>
2018-01-07 20:16:51 -0500

    Add files via upload

---
+++ cassandra.js
@@ -0,0 +1,9 @@
+var cassandra=require("cassandra-driver");
+//var async=require(async);
+const client=new cassandra.Client({contactPoints:['localhost:9042'],keyspace:'sample'});
+const query='select *from employe';
+client.execute(query,function(err,result){
+if(err)
+	console.log(err)
+console.log(result.rows[0].name);
+});
